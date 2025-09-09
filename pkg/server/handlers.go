32b37ee93e2d282472a4eda734e1ad6e4d854f46
Xavier Coulon <xcoulon@redhat.com>
2025-09-09 15:14:47 +0200

    refactor: remove logger param in handler funcs
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -2,29 +2,25 @@ package server
 
 import (
 	"context"
-	"log/slog"
 
 	api "github.com/xcoulon/converse-mcp/pkg/api"
 )
 
-// TODO: remove logger
-type PromptHandleFunc func(ctx context.Context, logger *slog.Logger, params api.GetPromptRequestParams) (any, error)
+type PromptHandleFunc func(ctx context.Context, params api.GetPromptRequestParams) (any, error)
 
 type PromptHandler struct {
 	Prompt api.Prompt
 	Handle PromptHandleFunc
 }
 
-// TODO: remove logger
-type ResourceHandleFunc func(ctx context.Context, logger *slog.Logger, params api.ReadResourceRequestParams) (any, error)
+type ResourceHandleFunc func(ctx context.Context, params api.ReadResourceRequestParams) (any, error)
 
 type ResourceHandler struct {
 	Resource api.Resource
 	Handle   ResourceHandleFunc
 }
 
-// TODO: remove logger
-type ToolHandleFunc func(ctx context.Context, logger *slog.Logger, params api.CallToolRequestParams) (any, error)
+type ToolHandleFunc func(ctx context.Context, params api.CallToolRequestParams) (any, error)
 
 type ToolHandler struct {
 	Tool   api.Tool
